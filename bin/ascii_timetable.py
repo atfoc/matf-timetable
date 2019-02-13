93cc0ced21e2839c4e3fd9d2859158209891e839
Pedja Trifunov <trifunov.pedja@gmail.com>
2019-02-13 22:20:55 +0100

    Added callback for creating ascii timetable

---
+++ bin/ascii_timetable.py
@@ -0,0 +1,83 @@
+import logging
+import typing as ty
+import os
+from dashtable import data2rst
+from parser.parser import parser
+
+
+
+class callback:
+    def __init__(self, dir_name: str):
+        self.table = None
+        self.group = ''
+        self.dir_name = dir_name
+
+        self.reset()
+
+
+    def __call__(self, day:int, start: int, finish: int, sub: str, type: int, teacher: ty.Optional[str], group: str,
+                 sub_group: ty.Optional[str], room: str, hash: str)->None:
+
+
+        if self.group == '':
+            self.group = group
+        if self.group != group:
+            self.print()
+            self.group = group
+            self.reset()
+
+        if self.table[day+1][start-7] == '':
+            self.table[day+1][start-7] = sub + '\n' + (teacher if teacher is not None else '')
+        else:
+            if isinstance(self.table[day+1][start-7], list):
+                self.table[day+1][start-7].append([sub + '\n' + (teacher if teacher is not None else '')])
+            else:
+                self.table[day+1][start-7] = [[self.table[day+1][start-7]], [sub + '\n' + (teacher if teacher is not None else '')]]
+        if start != finish-1:
+            self.span.append([[day+1, i] for i in range(start-7, finish-7)])
+
+
+
+    def print(self):
+
+        if not os.path.exists(os.path.join('.', self.dir_name)):
+            os.mkdir(os.path.join('.', self.dir_name))
+
+        for i in range(len(self.table)):
+            for j in range(len(self.table[i])):
+                if isinstance(self.table[i][j], list):
+                    self.table[i][j] = data2rst(self.table[i][j], center_cells=True, use_headers=False)
+
+        with open(os.path.join('.', self.dir_name, f'{self.group}.txt'), 'w') as f:
+            f.write(data2rst(self.table, self.span, center_cells=True))
+
+    def reset(self):
+        self.table = [list(['' for i in range(14)]) for i in range(6)]
+        self.span = []
+        for i in range(13):
+            self.table[0][i+1] = str(8+i) + '-' + str(8+i+1)
+
+        self.table[1][0] = 'Ponedeljak'
+        self.table[2][0] = 'Utorak'
+        self.table[3][0] = 'Sreda'
+        self.table[4][0] = 'Cetvrtak'
+        self.table[5][0] = 'Petak'
+
+
+def main():
+    logging.basicConfig(filename='parser.log', level=logging.DEBUG)
+
+    p = parser('http://poincare.matf.bg.ac.rs/~kmiljan/raspored/sve/index.html', True)
+
+    c = callback('raspored')
+
+    p.parse(c)
+
+    c.print()
+
+
+
+
+
+if __name__ == '__main__':
+    main()
