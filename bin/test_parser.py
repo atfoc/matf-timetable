c00c03578f4c598c383896a5b9d99b5f9c89d8e2
Pedja Trifunov <trifunov.pedja@gmail.com>
2019-02-16 15:59:21 +0100

    Modified example to use new class for timetable data

--- bin/test_parser.py
+++ bin/test_parser.py
@@ -1,31 +1,23 @@
 import logging
 import typing as ty
-from fileinput import filename
 
 from parser.parser import parser
-from parser.parser import lecture_enum
+from parser.timetable_unit import timetable_unit
 
 
 
-def callback(day:int, start: int, finish: int, sub: str, type: int, teacher: ty.Optional[str], group: str,
-             sub_group: ty.Optional[ty.List[str]], room: str, hash: str)->None:
+def callback(unit: timetable_unit)->None:
 
-    if type == lecture_enum.LECTURE:
-        type_name = 'Predavanje'
-    elif type == lecture_enum.PRACTICAL:
-        type_name = 'Vezbe'
-    else:
-        type_name = 'Praktikum'
 
-    print(f'Day: {day}')
-    print(f'Time: {start}-{finish}')
-    print(f'Subject: {sub}')
-    print(f'Type: {type_name}')
-    print(f'Teacher: {teacher if teacher is not None else ""}')
-    print(f'Group: {group}')
-    print(f'Sub_group: {sub_group if sub_group is not None else ""}')
-    print(f'Room : {room}')
-    print(f'Hash: {hash}')
+    print(f'Day: {unit.day}')
+    print(f'Time: {unit.start_time}-{unit.finish_time}')
+    print(f'Subject: {unit.subject}')
+    print(f'Type: {unit.type_text()}')
+    print(f'Teacher: {unit.teacher_safe()}')
+    print(f'Group: {unit.group}')
+    print(f'Sub_group: {unit.sub_group_safe()}')
+    print(f'Room : {unit.room}')
+    print(f'Hash: {unit.hash}')
     print('------------------------------------------------------------------------------')
 
 
