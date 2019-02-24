881072c22da82a5e1ad240e35f5ebeaef830b1f2
Vuk Novakovic <vnovakovic96@gmail.com>
2019-02-24 02:16:09 +0100

    refactor
    moved FormulaGenerator to formula_generator.py
    created main.py with main function

--- bin/solvers/formula/formula_generator.py
+++ bin/solvers/formula/formula_generator.py
@@ -8,12 +8,10 @@ from utils.html_writer import timetable_to_html
 from dimacs_coder import DimacsCoder
 from logic_classes import literal, disjunction
 import typing as ty
-import subprocess
 
 def sqlite_connection_str(path: str)->str:
     """ :param path - absolute path to db or :memory: for in memory db"""
     return f'sqlite:///{path}'
-
 
 class FormulaGenerator:
     def __init__(self, filepath : str):
@@ -159,67 +157,3 @@ class FormulaGenerator:
         self.__map_courses_to_units()
         self.__YOLO()
         self.__no_course_overlap()
-        
-
-
-def main():
-
-    #TODO razdvojiti prve i druge vezbe(ako postoje)
-
-    fg = FormulaGenerator("test_input.txt")
-
-    num_of_results = 0
-    unsat_count = 0
-    fg.code()
-    c = DimacsCoder(fg.formula)
-    dimacs_string = c.create_dimacs_string()
-
-    while True:
-        if unsat_count > 1000 or num_of_results > 100:
-            break
-        dimacs_in = open("out.cnf", "w")
-        dimacs_out = open("solution.cnf", "r")
-
-        dimacs_in.writelines(dimacs_string)
-        dimacs_in.flush()
-
-        subprocess.run(["minisat", "out.cnf", "solution.cnf"])
-        dimacs_in.close()
-
-        is_solved = dimacs_out.readline().strip() == "SAT"
-        if is_solved:
-            solution = dimacs_out.readline()
-            solution = solution.split(" ")
-            solution = solution[:-1] #all but last 0
-            res = c.decode_dimacs_string(solution)
-            with open(f"output/result{num_of_results}.html", "w") as html_file:
-                html_file.writelines(timetable_to_html(res) )
-
-            num_of_results += 1
-
-            #increase number of clauses
-            tmp = dimacs_string.split("\n")
-            num_of_clauses = int(tmp[0].split(" ")[-1]) + 1
-            num_of_vars = int(tmp[0].split(" ")[2])
-            dimacs_string = f"p cnf {num_of_vars} {num_of_clauses}\n" + "\n".join(tmp[1:])
-
-            #we forbid current solution so we get other combinations
-            tmp : str = ''
-            for s in solution:
-                if s[0] == '-':#if negated
-                    tmp += s[1:] + ' '#then add positive
-                else: #else add negated
-                    tmp += '-' + s + ' '
-            
-            dimacs_string += tmp + '0\n'
-        else:
-            fg.remove_unit()
-            fg.code()
-            c = DimacsCoder(fg.formula)
-            dimacs_string = c.create_dimacs_string()
-            unsat_count += 1
-            print("UNSAT")
-        dimacs_out.close()
-
-if __name__ == "__main__":
-    main()
---
+++ bin/solvers/formula/main.py
@@ -0,0 +1,67 @@
+from formula_generator import FormulaGenerator
+from dimacs_coder import DimacsCoder
+import typing as ty
+from utils.html_writer import timetable_to_html
+import subprocess
+
+def main():
+
+    #TODO razdvojiti prve i druge vezbe(ako postoje)
+
+    fg = FormulaGenerator("test_input.txt")
+
+    num_of_results = 0
+    unsat_count = 0
+    fg.code()
+    c = DimacsCoder(fg.formula)
+    dimacs_string = c.create_dimacs_string()
+
+    while True:
+        if unsat_count > 1000 or num_of_results > 100:
+            break
+        dimacs_in = open("out.cnf", "w")
+        dimacs_out = open("solution.cnf", "r")
+
+        dimacs_in.writelines(dimacs_string)
+        dimacs_in.flush()
+
+        subprocess.run(["minisat", "out.cnf", "solution.cnf"])
+        dimacs_in.close()
+
+        is_solved = dimacs_out.readline().strip() == "SAT"
+        if is_solved:
+            solution = dimacs_out.readline()
+            solution = solution.split(" ")
+            solution = solution[:-1] #all but last 0
+            res = c.decode_dimacs_string(solution)
+            with open(f"output/result{num_of_results}.html", "w") as html_file:
+                html_file.writelines(timetable_to_html(res) )
+
+            num_of_results += 1
+
+            #increase number of clauses
+            tmp = dimacs_string.split("\n")
+            num_of_clauses = int(tmp[0].split(" ")[-1]) + 1
+            num_of_vars = int(tmp[0].split(" ")[2])
+            dimacs_string = f"p cnf {num_of_vars} {num_of_clauses}\n" + "\n".join(tmp[1:])
+
+            #we forbid current solution so we get other combinations
+            tmp : str = ''
+            for s in solution:
+                if s[0] == '-':#if negated
+                    tmp += s[1:] + ' '#then add positive
+                else: #else add negated
+                    tmp += '-' + s + ' '
+            
+            dimacs_string += tmp + '0\n'
+        else:
+            fg.remove_unit()
+            fg.code()
+            c = DimacsCoder(fg.formula)
+            dimacs_string = c.create_dimacs_string()
+            unsat_count += 1
+            print("UNSAT")
+        dimacs_out.close()
+
+if __name__ == "__main__":
+    main()
